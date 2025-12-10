edbfb62658c250fc777e229471a2ff942f4353e8
Leo <xiang.tao@outlook.de>
2025-12-10 21:21:39 +0800

    1

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ func main() {
 	err := installer.CreateInstaller(
 		"./installer/stub/stub.exe",
 		"./yuumi.exe",
-		"./lol_yuumi_setup_v091.exe",
+		"./lol_yuumi_setup_v092.exe",
 		installer.Options{
 			ProductName:             "lolyuumi",
 			ExeName:                 "yuumi.exe", // 若你的真实文件名不同，改这里
 			CreateDesktopShortcut:   true,
 			CreateStartMenuShortcut: true,
-			Version:                 "0.9.1",
+			Version:                 "0.9.2",
 			ShortcutName:            "悠米助手纯净版",
 			CompressionLevel:        gzip.BestCompression, // 使用最高压缩级别
 		},
